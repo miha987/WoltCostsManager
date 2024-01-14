63ad8f1045c6bb34f2ae9ddf4aa452ccd8225be3
miha987 <zadravecmiha987@gmail.com>
2024-01-14 17:06:23 +0100

    wolt manager v4

---
+++ background.js
@@ -0,0 +1,202 @@
+// TODO add to config!!
+let apiKey = 'ZpUkk7SlvqgayWf9AP2exevkjVAEGbWjjfofeDs9';
+// let groupId = 33000588; // datastat
+let groupId = 32922268; // test
+let splitwiseBaseUrl = 'https://secure.splitwise.com/api/v3.0';
+
+chrome.runtime.onInstalled.addListener(() => {
+	console.log('TEST!');
+});
+
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+	console.log("TUKI!");
+	console.log(data);
+
+    if (data.type == 'getGroupUsers') {
+        console.log("GETING GROUP USERS");
+        getUsersFromSplitwiseGroup();
+    }
+    
+    if (data.type == 'settleCosts') {
+        console.log("SETTLING COSTS IN THE BACKGROUND");
+        settleCosts(data.costsObj);
+    }
+
+    sendResponse();
+});
+
+function settleCosts(costs) {
+	console.log("BACK COSTS", costs);
+
+	let deliveryShare = null;
+	let deliveryShareReminder = null;
+	let uniquePersonsIds = null;
+
+	if (costs.serviceFee) 
+		costs.deliveryFee = (parseFloat(costs.deliveryFee) + parseFloat(costs.serviceFee)).toString();
+
+	if (costs.deliveryFee) {
+		let personsIdsArr = costs.paid_for.map(o => o.id*1);
+		// personsIdsArr.push(costs.paid_by_person_id*1);
+
+		uniquePersonsIds = personsIdsArr.filter((value, index, arr) => {
+			return arr.indexOf(value) === index;
+		});
+
+		uniquePersonsIds = uniquePersonsIds.filter(id => id != -1);
+
+		console.log("UNIQUE IDS", uniquePersonsIds);
+		deliveryShareFloat = (parseFloat(costs.deliveryFee) / uniquePersonsIds.length).toFixed(2);
+		deliveryShare = deliveryShareFloat.toString();
+
+		// let deliveryShareReminder = parseFloat(costs.deliveryFee) - (deliveryShareFloat * uniquePersonsIds.length);
+		let deliveryShareMultipleRounded = Math.round(((deliveryShareFloat * uniquePersonsIds.length) + Number.EPSILON) * 100) / 100;
+		deliveryShareReminder =  (parseFloat(costs.deliveryFee) - deliveryShareMultipleRounded + Number.EPSILON).toFixed(2);
+		console.log("DELIVERY SHARE", deliveryShare);
+		console.log("DELIVERY SHARE MULTIPLIED", deliveryShareMultipleRounded);
+		console.log("DELIVERY SHARE REMINDER", deliveryShareReminder);
+
+	}
+
+	let numberOfRequestsMade = 0;
+	let numberOfRequestsToMake = costs.paid_for.filter(o => o.id*1 != costs.paid_by_person_id*1 && o.id*1 != -1).length;
+
+	if (deliveryShare)
+		numberOfRequestsToMake += 1;
+
+
+	costs.paid_for.forEach(order =>  {
+		if (order.id*1 == costs.paid_by_person_id*1 || order.id*1 == -1)
+			return;
+
+		let expanseObj = {
+			cost: order.price,
+			description: `Wolt order - ${order.itemName}`,
+			repeat_interval: "never",
+			currency_code: "EUR",
+			group_id: groupId,
+			users__0__user_id: order.id,
+			users__0__paid_share: "0",
+			users__0__owed_share: order.price,
+			users__1__user_id: costs.paid_by_person_id,
+			users__1__paid_share: order.price,
+			users__1__owed_share: "0",
+		};
+
+		makeSplitwisePOSTRequest(`/create_expense`, expanseObj, resp => {
+			console.log("EXPANSE RESP", resp);
+			iterateSettleCostsRequests();
+		});
+
+	});
+
+	if (deliveryShare) {
+		console.log("INCLUDED", uniquePersonsIds.includes(costs.paid_by_person_id*1));
+
+		let deliveryExpanseObj = {
+			cost: costs.deliveryFee,
+			description: "Wolt order delivery fee",
+			repeat_interval: "never",
+			currency_code: "EUR",
+			group_id: groupId,
+			users__0__user_id: costs.paid_by_person_id,
+			users__0__paid_share: costs.deliveryFee,
+			users__0__owed_share: uniquePersonsIds.includes(costs.paid_by_person_id*1) ? deliveryShare : '0'
+		};
+
+		let user_counter = 1;
+
+		for (var i = 0; i < uniquePersonsIds.length; i++) {
+			if (costs.paid_by_person_id*1 == uniquePersonsIds[i])
+				continue;
+
+			deliveryExpanseObj[`users__${user_counter}__user_id`] = uniquePersonsIds[i];
+			deliveryExpanseObj[`users__${user_counter}__paid_share`] = '0';
+			deliveryExpanseObj[`users__${user_counter}__owed_share`] = user_counter > 1 ? deliveryShare : (deliveryShare*1 + deliveryShareReminder*1).toString();
+			
+			console.log("USER PAID", deliveryExpanseObj[`users__${user_counter}__owed_share`]);
+
+			user_counter += 1;
+		}
+
+		console.log("deliveryExpanseObj", deliveryExpanseObj);
+
+		makeSplitwisePOSTRequest(`/create_expense`, deliveryExpanseObj, resp => {
+			console.log("EXPANSE RESP", resp);
+			iterateSettleCostsRequests();
+		});
+	}
+
+	function iterateSettleCostsRequests() {
+		numberOfRequestsMade +=1;
+
+		console.log(`MADE ${numberOfRequestsMade}/${numberOfRequestsToMake} REQUESTS`);
+
+		if (numberOfRequestsMade == numberOfRequestsToMake)
+			finishedSettlingCosts()
+	}
+}
+
+async function finishedSettlingCosts() {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+	chrome.tabs.sendMessage(tab.id, {
+		type: "finishedSettleCosts"
+	});
+}
+
+function getUsersFromSplitwiseGroup() {
+	makeSplitwiseGETRequest(`/get_group/${groupId}`, async resp => {
+		console.log(resp);
+		let groupUsers = resp.group.members.map(member => {
+			return {
+				id: member.id,
+				name: `${member.first_name ? member.first_name : ''} ${member.last_name ? member.last_name : ''}`
+			}
+		});
+
+		console.log("GROUP MEMBERS", groupUsers);
+
+		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+		chrome.tabs.sendMessage(tab.id, {
+			type: "setGroupUsers",
+			groupUsers: groupUsers
+		});
+
+	});
+}
+
+function makeSplitwiseGETRequest(path, callback)
+{
+	let url = `${splitwiseBaseUrl}${path}`;
+
+	fetch(url, {
+		method: "GET",
+		headers: {
+			"Authorization": `Bearer ${apiKey}`,
+			"Content-type": "application/json;charset=UTF-8"
+		}
+	})
+	.then(response => response.json()) 
+	.then(json => callback(json)) 
+	.catch(err => console.log(err));
+}
+
+function makeSplitwisePOSTRequest(path, payload, callback)
+{
+	let url = `${splitwiseBaseUrl}${path}`;
+
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"Authorization": `Bearer ${apiKey}`,
+			"Content-type": "application/json;charset=UTF-8",
+			'Accept': 'application/json',
+		},
+		body: JSON.stringify(payload)
+	})
+	.then(response => response.json()) 
+	.then(json => callback(json)) 
+	.catch(err => console.log(err));
+}
--- content_scripts/costs_manager.js
+++ content_scripts/costs_manager.js
@@ -1,58 +1,206 @@
 (function() {
-	let privateGroup = [
-		{
-			id: 1,
-			name: "Miha"
-		},
-		{
-			id: 2,
-			name: "Tomaž"
-		},
-		{
-			id: 3,
-			name: "Vanesa"
-		},
-		{
-			id: -1,
-			name: "Other"
-		},
-	];
+	let groupUsers = [];
 
+	let groupUsersSet = false;
 	let componentInitialized = false;
+	let onCheckout = false;
 
-	function generatePayForPersonDropdownHTML(price) {
-		return `<select data-price="${price}" class="pay-for-person-select" style="border: 1px solid #e4e4e4; cursor: pointer; margin-top: 10px;">
-				${(privateGroup.map(person => {
-					return `<option value="${person.id}">${person.name}</option>`;
-				})).join('')}
-		</select>`;
+	let numberOfSplitsMapping = {};
+
+
+	function getUsersFromSplitwiseGroup() {
+	    chrome.runtime.sendMessage({type: "getGroupUsers"});
 	}
 
-	function generatePayForPersoDropdownElement(price) {
+	function generatePayForPersonDropdownHTML(price, itemName, canBeRemoved=false) {
+		return `<select data-price="${price}" data-item_name="${itemName}" class="pay-for-person-select" style="border: 1px solid #e4e4e4; cursor: pointer; margin-top: 10px;">
+				${(groupUsers.map(person => {
+					return `<option value="${person.id}">${person.name}</option>`;
+				})).join('')}
+		</select>
+		<div class="pay-for-person-price-label" style="margin-top: 10px; margin-left: 7px;">${price}€</div>
+		${canBeRemoved ? '<div class="pay-for-person-split-cancel" style="background: #E74C3C; margin-top: 10px; margin-left: 7px; border-radius: 5px; padding: 1px 7px; color: white; font-size: 14px; font-weight: 700; cursor: pointer;">X</div>' : '' }`;
+	}
+
+	function generatePayForPersoDropdownElement(costsDropdownsContainer, price, itemName, canBeRemoved) {
 		let payForPersonContainerElement = document.createElement("div");
 		payForPersonContainerElement.classList.add('pay-for-person-container');
 
-		let payForPersonDropdownHTML = generatePayForPersonDropdownHTML(price);
+		payForPersonContainerElement.style.display = 'flex';
+		payForPersonContainerElement.style.alignItems = 'center';
+
+		console.log("THIS PRICE?1", price);
+
+		let payForPersonDropdownHTML = generatePayForPersonDropdownHTML(price, itemName, canBeRemoved);
 
 		payForPersonContainerElement.innerHTML = payForPersonDropdownHTML;
+
+		if (canBeRemoved) {
+			let cancelSplitBtn = payForPersonContainerElement.querySelector('.pay-for-person-split-cancel');
+
+			cancelSplitBtn.addEventListener('click', e => {
+				e.preventDefault();
+				e.stopPropagation();
+
+				removeCostSplitFromItem(costsDropdownsContainer, payForPersonContainerElement);
+			});
+		}
 
 		return payForPersonContainerElement;
 	}
 
+	function generateItemCostsContainerElement(price, itemName, splitByNum) {
+		let itemCostsContainerElement = document.createElement("div");
+		itemCostsContainerElement.classList.add('item-costs-container');
+
+		itemCostsContainerElement.style.borderBottom = '2px solid #e4e4e4';
+		itemCostsContainerElement.style.paddingBottom = '10px';
+
+		itemCostsContainerElement.dataset.price = price;
+
+		let costsDropdownsContainer = document.createElement("div");
+		costsDropdownsContainer.classList.add('costs-dropdowns-container');
+
+		for (var i = 0; i < splitByNum; i++) {
+			console.log("THIS PRICE?2", price);
+			let payForPersonContainerElement = generatePayForPersoDropdownElement(costsDropdownsContainer, price, itemName, false);
+			costsDropdownsContainer.appendChild(payForPersonContainerElement)
+		}
+
+		itemCostsContainerElement.appendChild(costsDropdownsContainer);
+
+		let splitCostsBtnElement = document.createElement("div");
+		
+		splitCostsBtnElement.classList.add('split-costs-btn');
+		splitCostsBtnElement.innerHTML = 'Split cost';
+		// splitCostsBtnElement.style.textAlign = 'center';
+		// splitCostsBtnElement.style.padding = '10px 0px';
+		splitCostsBtnElement.style.textAlign =  'center';
+		splitCostsBtnElement.style.padding =  '3px 10px';
+		splitCostsBtnElement.style.background =  '#1fa9e4';
+		splitCostsBtnElement.style.cursor =  'pointer';
+		splitCostsBtnElement.style.borderRadius =  '8px';
+		splitCostsBtnElement.style.fontSize =  '14px';
+		splitCostsBtnElement.style.fontWeight =  '500';
+		splitCostsBtnElement.style.color =  '#fff';
+		splitCostsBtnElement.style.margin =  '12px auto 3px';
+		splitCostsBtnElement.style.width =  'fit-content';
+
+		splitCostsBtnElement.addEventListener('click', e => {
+			e.preventDefault();
+			e.stopPropagation();
+
+			addCostSplitToItem(itemCostsContainerElement, itemName);
+		});
+
+		itemCostsContainerElement.appendChild(splitCostsBtnElement);
+
+		return itemCostsContainerElement;
+	}
+
+	function calculateCostsForItem(itemCostsContainer) {
+		let itemDropdownContainers = itemCostsContainer.querySelectorAll('.pay-for-person-container');
+		// let itemDropdownContainers = itemCostsContainer.querySelectorAll('.pay-for-person-select');
+
+		let itemPrice = itemCostsContainer.dataset.price;
+		let numberOfSplits = itemDropdownContainers.length;
+
+		let priceShareFloat = (parseFloat(itemPrice*1) / numberOfSplits).toFixed(2);
+		let priceShare = priceShareFloat.toString();
+
+		let priceShareMultipleRounded = Math.round(((priceShareFloat * numberOfSplits) + Number.EPSILON) * 100) / 100;
+		let priceShareReminder =  (parseFloat(itemPrice) - priceShareMultipleRounded + Number.EPSILON).toFixed(2);
+
+		console.log("CONTAINERS", itemDropdownContainers);
+
+		for (var i = 0; i < itemDropdownContainers.length; i++) {
+			let itemDropdown = itemDropdownContainers[i].querySelector('.pay-for-person-select');
+			let itemPriceLabel = itemDropdownContainers[i].querySelector('.pay-for-person-price-label');
+			console.log("DD", itemDropdown);
+			console.log("LB", itemPriceLabel);
+
+			let newPrice = priceShare;
+
+			if (i == 0)
+				newPrice = (priceShare*1 + priceShareReminder*1).toFixed(2).toString();
+
+			console.log("NEW PRICEE!", newPrice);
+
+			itemDropdown.dataset.price = newPrice;
+			itemPriceLabel.innerHTML = `${newPrice}€`;
+		}
+	}
+
+	function addCostSplitToItem(itemCostsContainer, itemName) {
+		let costsDropdownsContainer = itemCostsContainer.querySelector('.costs-dropdowns-container');
+		let payForPersonContainerElement = generatePayForPersoDropdownElement(itemCostsContainer, 1, itemName, true);
+
+		payForPersonContainerElement.querySelector('.pay-for-person-select').addEventListener('click', e => {
+			e.preventDefault();
+			e.stopPropagation();
+		});
+
+		costsDropdownsContainer.appendChild(payForPersonContainerElement);
+
+		calculateCostsForItem(itemCostsContainer);
+	}
+
+	function removeCostSplitFromItem(itemCostsContainer, payForPersonContainerElement) {
+		let costsDropdownsContainer = itemCostsContainer.querySelector('.costs-dropdowns-container');
+
+		costsDropdownsContainer.removeChild(payForPersonContainerElement);
+
+		calculateCostsForItem(itemCostsContainer);
+	}
+
+
 	function generatePayForPersonDropdownsElements() {
-		let orderedItems = document.querySelectorAll('[data-test-id="MenuItemContent"]');
+		let orderedItems = document.querySelectorAll('[data-test-id="CartItem"]');
 
 		for (var i = 0; i < orderedItems.length; i++) {
 			let itemElement = orderedItems[i];
 			
-			let priceElement = itemElement.querySelector('[data-test-id="menu-item-presentational.price"]');
-			let price = priceElement.innerHTML.replaceAll('€', '')*1;
+			// let priceElement = itemElement.querySelector('[data-test-id="menu-item-presentational.price"]');
+			
+			let priceElement = itemElement.querySelector('[data-test-id="CartItemName"]').nextElementSibling.firstChild;
 
-			let payForPersonContainerElement = generatePayForPersoDropdownElement(price);
+			let price = priceElement.innerHTML.replaceAll('€', '').replaceAll(',', '.').replaceAll(' ', '').replaceAll('&nbsp;', '').trim();
 
-			itemElement.appendChild(payForPersonContainerElement)
+			// console.log("AAAA", priceElement.innerHTML);
 
-			console.log("CENA", price);
+
+			let itemNameElement = itemElement.querySelector('[data-test-id="CartItemName"]');
+			// let itemName = itemNameElement.childNodes[1].textContent;
+			let itemName = itemNameElement.innerHTML;
+
+			console.log("NAME", itemName);
+			
+			
+			let numberOfOrderedItemsElement = itemElement.querySelector('[data-test-id="CartItemStepperValue"]');
+			// let numberOfOrderedItems = numberOfOrderedItemsElement.dataset.value*1;
+			let numberOfOrderedItems = numberOfOrderedItemsElement.innerHTML.trim()*1;
+			
+			console.log("SSS", numberOfOrderedItems);
+			
+			// TODO!! HANDLE  UNEQUAL SPLITS!!
+			let singleItemPrice = (price*1) / numberOfOrderedItems*1;
+			
+			console.log("PRICE", price);
+			console.log("SINGLE ITEM PRICE", singleItemPrice);
+			let costsContainerElement = document.createElement("div");
+			costsContainerElement.classList.add('costs-container');
+
+			for (var j = 0; j < numberOfOrderedItems; j++) {
+				// let payForPersonContainerElement = generatePayForPersoDropdownElement(singleItemPrice, itemName);
+				let itemCostsContainerElement = generateItemCostsContainerElement(singleItemPrice, itemName, 1);
+
+				costsContainerElement.appendChild(itemCostsContainerElement)
+			}
+			
+			itemElement.appendChild(costsContainerElement)
+
+
+			// console.log("CENA", price);
 		}
 
 		let personsSelects = document.querySelectorAll('.pay-for-person-select');
@@ -70,7 +218,7 @@
 
 	function generatePayByPersonDropdownHTML() {
 		return `<select id="pay-by-person-select" style="border: 1px solid #e4e4e4; cursor: pointer; margin: 5px 0px 10px 0px">
-				${(privateGroup.map(person => {
+				${(groupUsers.map(person => {
 					return `<option value="${person.id}">${person.name}</option>`;
 				})).join('')}
 		</select>`;
@@ -78,6 +226,8 @@
 
 	function generatePayByPersonComponent() {
 		let selectedItemsHElementContainer = document.querySelector('[data-test-id="OrderItemsList"] h3').parentElement;
+
+		console.log("selectedItemsHElementContainer");
 
 		let payByPersonComponentContainerElement = document.createElement("div");
 		payByPersonComponentContainerElement.classList.add('pay-by-person-container');
@@ -99,7 +249,9 @@
 	}
 
 	function generateSettleCostsButton() {
+		// let orderItemsListElement = document.querySelector('[data-test-id="TitledItemsList"]');
 		let orderItemsListElement = document.querySelector('[data-test-id="OrderItemsList"]');
+		console.log("MIHAAAAA!!");
 
 		let settleCostsButtonContainerElement = document.createElement("div");
 		settleCostsButtonContainerElement.id = 'settle-costs-btn';
@@ -138,6 +290,7 @@
 		let settleCostsBtnParent = settleCostsBtn.parentElement;
 
 		let loadingElement = document.createElement('div');
+		loadingElement.id = 'settle-costs-loading';
 		loadingElement.innerHTML = generateLoadingDivHTML();
 
 		settleCostsBtn.remove();
@@ -147,20 +300,46 @@
 
 		console.log("PAID BY", payByPersonId);
 
+		let costsObj = {
+			paid_by_person_id: payByPersonId,
+			paid_for: [],
+			deliveryFee: null,
+			serviceFee: null,
+		}
+
 		let personsSelects = document.querySelectorAll('.pay-for-person-select');
 		for (var i = 0; i < personsSelects.length; i++) {
 			let personId = personsSelects[i].value;
 			let personsPrice = personsSelects[i].dataset.price;
+			let personsItemName = personsSelects[i].dataset.item_name;
 
-			console.log("PAYING FOR", personId, personsPrice);
+			costsObj.paid_for.push({
+				id: personId,
+				price: personsPrice,
+				itemName: personsItemName
+			});
 		}
 
 
-		let costsSettledElement = document.createElement('div');
-		costsSettledElement.innerHTML = generateCostsSettledDivHTML();
+		let deliveryFeeElement = document.querySelector('[data-test-id="order.checkout-delivery-default"] span');
+		if (deliveryFeeElement)
+			costsObj.deliveryFee = deliveryFeeElement.innerHTML;
 
-		loadingElement.remove();
-		settleCostsBtnParent.appendChild(costsSettledElement);
+
+		let serviceFeeElement = document.querySelector('[class^="OrderSummary-module__priceItem"]');
+		if (serviceFeeElement)
+			costsObj.serviceFee = serviceFeeElement.innerHTML;
+
+
+		console.log("COSTS OBJ", costsObj);
+
+		chrome.runtime.sendMessage({
+			type: "settleCosts",
+			costsObj: costsObj
+		});
+
+
+		
 
 	}
 
@@ -168,22 +347,90 @@
 		console.log("INITIALIZING WOLT COMPONENT...");
 		componentInitialized = true;
 
+
 		generatePayByPersonComponent();
 		generatePayForPersonDropdownsElements();
 		generateSettleCostsButton();
 	}
+		
+	getUsersFromSplitwiseGroup();
 
 	setInterval(() => {
+		// console.log("LOCATION PATHNAME", window.location.pathname);
+		onCheckout = window.location.pathname.includes('/checkout');
+
+		if (!onCheckout)
+			return;
+
 		if (componentInitialized)
 			return;
 
-		let orderedItems = document.querySelectorAll('[data-test-id="MenuItemContent"]');
+		let orderedItems = document.querySelectorAll('[data-test-id="CartItem"]');
 
 		if (orderedItems.length == 0) {
 			console.log("NO ELEMENTS YET, SKIPING...");
 			return;
 		}
 
+		if (!groupUsersSet) {
+			console.log("USERS NOT SET YET, SKIPING...");
+			return;
+		}
+
 		initComponent();
 	}, 300);
+
+	// window.addEventListener('popstate', function (event) {
+	// 	console.log("PAGE CHANGED!");
+	// 	console.log(event.state);
+	// });
+
+	console.log("TUKI!");
+
+	chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+	    if (data.type == 'setGroupUsers') {
+	        console.log("SETTING GROUP USERS");
+	        groupUsers = data.groupUsers;
+
+	        // groupUsers = groupUsers.sort((a, b) => a.name.toUpperCase() - b.name.toUpperCase());
+
+	        groupUsers.sort(function(a, b) {
+				const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+				const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+
+				// names must be equal
+				return 0;
+			});
+
+	        console.log("GG", groupUsers);
+
+	        groupUsers.push({
+	        	id: -1,
+	        	name: 'Other'
+	        })
+
+	        groupUsersSet = true;
+	    }
+
+	    if (data.type == 'finishedSettleCosts') {
+	    	console.log("COSTS SETTLED!!");
+	    	
+	    	let costsSettledElement = document.createElement('div');
+			costsSettledElement.innerHTML = generateCostsSettledDivHTML();
+
+			let loadingElement = document.getElementById('settle-costs-loading');
+			let settleCostsBtnParent = loadingElement.parentElement;
+
+			loadingElement.remove();
+			settleCostsBtnParent.appendChild(costsSettledElement);
+	    }
+
+	    sendResponse();
+	});
 })();
